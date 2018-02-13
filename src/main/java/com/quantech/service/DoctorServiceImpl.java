83ff1d9e2fbc0764638ae60000208caa2f457da6
nuhat <nt6888@bristol.ac.uk>
2018-02-13 17:35:41 +0000

    adding some TODO reminders

--- src/main/java/com/quantech/service/DoctorServiceImpl.java
+++ src/main/java/com/quantech/service/DoctorServiceImpl.java
@@ -12,66 +12,77 @@ import java.util.function.Predicate;
 public class DoctorServiceImpl implements DoctorService {
     @Override
     public List<Doctor> getAllDoctors() {
+        // TODO
         return null;
     }
 
     @Override
     public List<Doctor> sortDoctorsByDateRenewedMostRecentFirst(List<Doctor> list) {
+        // TODO
         return null;
     }
 
     @Override
     public List<Doctor> sortDoctorsByDateRenewedMostRecentLast(List<Doctor> list) {
+        // TODO
         return null;
     }
 
     @Override
     public List<Doctor> sortDoctorsByFirstName(List<Doctor> list) {
+        // TODO
         return null;
     }
 
     @Override
     public List<Doctor> sortDoctorsByLastName(List<Doctor> list) {
+        // TODO
         return null;
     }
 
     @Override
     public Doctor getDoctor(UserCore user) {
+        // TODO
         return null;
     }
 
     @Override
     public void saveDoctor(Doctor doctor) throws NullPointerException, IllegalArgumentException {
-
+        // TODO
     }
 
     @Override
     public void deleteDoctor(Long id) {
-
+       // TODO
     }
 
     @Override
     public List<Patient> getPatientsUnderCareOf(UserCore user) {
+        // TODO
         return null;
     }
 
     @Override
     public List<Doctor> filterDoctorsBy(List<Doctor> list, Predicate<Doctor> predicate) {
+        // TODO
         return null;
     }
 
     @Override
     public List<Doctor> filterDoctorsBy(List<Doctor> list, Iterable<Predicate<Doctor>> predicates) {
+        // TODO
         return null;
     }
 
     @Override
     public Predicate<Doctor> doctorsFirstNameStartsWith(String str) {
+        // TODO
         return null;
     }
 
     @Override
     public Predicate<Doctor> doctorsLastNameStartsWith(String str) {
+        // TODO
         return null;
     }
 }
