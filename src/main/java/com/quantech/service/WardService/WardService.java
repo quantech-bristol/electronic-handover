30b818d321ad7904185f9225af3974d4bf02862b
Thomas Walker <32672682+walker-tom@users.noreply.github.com>
2018-02-13 19:07:59 +0000

    ward service renamed

--- src/main/java/com/quantech/service/WardService/WardService.java
+++ src/main/java/com/quantech/service/WardService/WardService.java
@@ -1,52 +1,36 @@
 package com.quantech.service.WardService;
 
 import com.quantech.model.Ward;
-import com.quantech.repo.WardRepository;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Service;
 
 import java.util.ArrayList;
 import java.util.List;
 
-@Service("wardService")
-public class WardService {
-
-    @Autowired
-    WardRepository wardRepository;
+public interface WardService
+{
 
     /**
      * Finds all wards currently stored in the repository.
      * @return A list of all wards currently stored, potentially corresponding to all wards in a hospital.
      */
-    public List<Ward> getAllWards() {
-        List<Ward> wards = new ArrayList<>();
-        wardRepository.findAll().forEach(wards::add);
-        return wards;
-    }
+    public List<Ward> getAllWards() ;
 
     /**
      * Finds a ward corresponding to the unique id, stored in the repository.
      * @param id The id for which a ward is a associated with.
      * @return A ward corresponding to the id if one exists, null otherwise.
      */
-    public Ward getWard(Long id) {
-        return wardRepository.findOne(id);
-    }
+    public Ward getWard(Long id);
 
     /**
      * Save a given ward into the repository.
      * @param ward The ward to be saved.
      */
-    public void saveWard(Ward ward) {
-        wardRepository.save(ward);
-    }
+    public void saveWard(Ward ward);
 
     /**
      * Deletes a given ward from the repository.
      * @param id The id corresponding to the ward that is to be deleted.
      */
-    public void deleteWard(Long id) {
-        wardRepository.delete(id);
-    }
+    public void deleteWard(Long id);
 
 }
