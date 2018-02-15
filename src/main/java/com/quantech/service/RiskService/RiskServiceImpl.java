ae85b9e1c852f9ad7ed8d2c17ef39044ab4a2f37
Thomas Walker <32672682+walker-tom@users.noreply.github.com>
2018-02-15 18:44:00 +0100

    risk service implemented

--- src/main/java/com/quantech/service/RiskService/RiskServiceImpl.java
+++ src/main/java/com/quantech/service/RiskService/RiskServiceImpl.java
@@ -1,32 +1,36 @@
 package com.quantech.service.RiskService;
 
 import com.quantech.model.Risk;
+import com.quantech.repo.RiskRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class RiskServiceImpl implements RiskService {
 
+    @Autowired
+    RiskRepository riskRepository;
+
     @Override
     public List<Risk> getAllRisks() {
-        // TODO
-        return null;
+        List<Risk> risks = new ArrayList<>();
+        riskRepository.findAll().forEach(risks::add);
+        return risks;
     }
 
     @Override
     public Risk getRisk(Long id) {
-        // TODO
-        return null;
+        return riskRepository.findOne(id);
     }
 
     @Override
     public void saveRisk(Risk risk) {
-        // TODO
-
+        riskRepository.save(risk);
     }
 
     @Override
     public void deleteRisk(Long id) {
-        // TODO
-
+        riskRepository.delete(id);
     }
 }
