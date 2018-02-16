052c5a4b6d076c5191a8705992a1d420cfe152d1
Louis Heath <lh16421@bristol.ac.uk>
2018-02-16 11:48:02 +0000

    added viewing of categories

--- src/main/java/com/quantech/service/CategoryService/CategoryServiceImpl.java
+++ src/main/java/com/quantech/service/CategoryService/CategoryServiceImpl.java
@@ -5,6 +5,9 @@ import com.quantech.repo.CategoryRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.util.ArrayList;
+import java.util.List;
+
 @Service("categoryService")
 public class CategoryServiceImpl {
     @Autowired
@@ -12,5 +15,11 @@ public class CategoryServiceImpl {
 
     public void saveCategory(Category category) throws NullPointerException, IllegalArgumentException {
         categoryRepository.save(category);
+    }
+
+    public List<Category> getAllCategories() {
+        List<Category> cs = new ArrayList<>();
+        categoryRepository.findAll().forEach(c -> cs.add(c));
+        return cs;
     }
 }
