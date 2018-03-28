89b280733ca755cf5048c4e2edd3fb1d9f6fc954
Tilly Woodfield <tillyrwoodfield@gmail.com>
2018-03-28 19:19:00 +0100

    merge commit

--- src/main/java/com/quantech/control/DoctorController.java
+++ src/main/java/com/quantech/control/DoctorController.java
@@ -21,6 +21,7 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
+import javax.transaction.Transactional;
 import java.util.HashSet;
 import java.util.List;
 import java.util.Set;
